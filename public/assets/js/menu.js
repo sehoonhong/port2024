cfab9b4ea2e93f92c01ace99c94d76f43de876f6
홍세훈 <sehoonhong@hongsehun-ui-MacBookAir.local>
2024-01-07 22:51:33 +0900

    수정 중

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,14 +1,14 @@
 export function menu() {
-    const headerToggle = document.getElementById("headerToggle");
-    const headerNav = document.querySelector(".header__nav");
+    const header02Toggle = document.getElementById("header02Toggle");
+    const header02Nav = document.querySelector(".header02__nav");
 
-    if(headerToggle){
-        headerToggle.addEventListener("click", () => {
-            headerNav.classList.toggle("show")
-            
-            headerToggle.getAttribute("aria-expanded") === "true" 
-            ?  headerToggle.setAttribute("aria-expanded", "false") 
-            :  headerToggle.setAttribute("aria-expanded", "true");
+    if(header02Toggle){
+        header02Toggle.addEventListener("click", () => {
+            header02Nav.classList.toggle("show");
+    
+            header02Toggle.getAttribute("aria-expanded") === "true" 
+            ? header02Toggle.setAttribute("aria-expanded", "false") 
+            : header02Toggle.setAttribute("aria-expanded", "true");
         });
     }
 }
